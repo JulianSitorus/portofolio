b8c962f8f316b4fef9f8c69977a1f8a49c35f18e
JulianSitorus <julian.marsangap@ti.ukdw.ac.id>
2024-07-10 03:24:56 +0700

    email

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ function openMenu(){
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
+
+// ================================= Email ======================================
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (e){
+    e.preventDefault();
+
+    const url = e.target.action;
+    const formData = new FormData(contactForm);
+
+    fetch(url, {
+        method: "POST",
+        body: formData,
+        mode: "no-cors",
+    })
+        .then(() => {
+            // url ty
+            window.location.href='/thankyou.html'
+        })
+        .catch((e) => alert("Error occused"));
+});
