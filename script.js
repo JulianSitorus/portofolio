fe317ecb4dbe934b4d34ef95c69bd36be8d36659
JulianSitorus <julian.marsangap@ti.ukdw.ac.id>
2024-07-13 20:09:53 +0700

    terbaru

--- script.js
+++ script.js
@@ -1,3 +1,92 @@
+// ================================= Particle ======================================
+
+particlesJS("particles", {
+    particles: {
+        number: {
+            value: 250,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#ffffff"
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#000000"
+            }
+        },
+        opacity: {
+            value: 0.8,
+            random: true,
+            animation: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0,
+                sync: false
+            }
+        },
+        size: {
+            value: 3,
+            random: true
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#5e3bee",
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+        }
+    },
+    interactivity: {
+        detectsOn: "canvas",
+        events: {
+            onHover: {
+                enable: true,
+                mode: "push"
+            },
+            onClick: {
+                enable: true,
+                mode: "push"
+            },
+            resize: true
+        },
+        modes: {
+            repulse: {
+                distance: 100,
+                duration: 0.4
+            },
+            push: {
+                particles_nb: 4
+            }
+        }
+    },
+    retina_detect: true
+});
+
+// ================================= menu ======================================
+window.addEventListener('scroll', function () {
+    var scrollTopButton = document.querySelector('.scroll-top');
+    if (this.window.pageYOffset > 200) {
+        scrollTopButton.style.display = 'block';
+    } else {
+        scrollTopButton.style.display = 'none';
+    }
+});
+
+
 function openMenu(){
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
@@ -23,3 +112,6 @@ contactForm.addEventListener("submit", function (e){
         })
         .catch((e) => alert("Error occused"));
 });
+
+
+       
